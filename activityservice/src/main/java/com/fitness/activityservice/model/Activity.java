c0dbb1f2a0f819848788997ec3205d63d9bf7128
NadPerz <nadijaviyaktha@gmail.com>
2026-02-24 00:38:02 +0530

    add the annotation for activity java file under the activity service

--- activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
+++ activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
@@ -1,19 +1,41 @@
 package com.fitness.activityservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 import java.time.LocalDateTime;
 import java.util.Map;
 
-public class Activity {
+@Document(collection = "activities")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 
+public class Activity {
+    @Id
     private String id;
     private String userId;
     private ActivityType type;
     private Integer duration;
     private Integer caloriesBurned;
     private LocalDateTime startTime;
+
+    @Field("metrics")
     private Map<String, Object> additionalMetrics;
+
+    @CreatedDate
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
     private LocalDateTime getCreatedAt;
+
+
 
 
 }
